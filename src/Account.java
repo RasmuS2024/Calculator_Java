23d9e95b8e1a139034bac5c7815703239c9b063a
RasmuS2024 <rasburkhanov@yandex.ru>
2025-12-17 22:35:18 +0300

    Added logic in functions

--- src/Account.java
+++ src/Account.java
@@ -1,11 +1,17 @@
 public class Account {
-    private int accountNumber;
-    private double balance;
+    private String accountNumber;
+    private double balance = 0;
     private Customer owner;
+
+    Account(String accountNumber, double balance, Customer owner) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.owner = owner;
+    }
 
     public boolean deposit(double amount) {
         if (amount > 0) {
-            balance += amount;
+            this.balance += amount;
             return true;
         } else {
             System.out.println("Сумма должна быть положительной!");
@@ -14,8 +20,8 @@ public class Account {
     }
 
     public boolean withdraw(double amount) {
-        if (balance > 0 && amount <= balance) {
-            balance -= amount;
+        if (this.balance > 0 && amount <= this.balance) {
+            this.balance -= amount;
             return true;
         } else {
             System.out.println("На счете не хватает средств!");
@@ -23,15 +29,34 @@ public class Account {
         }
     }
 
+    public boolean transfer(Account to, double amount) {
+        if (to == null) {
+            System.out.println("Ошибка: счет получателя не существует");
+            return false;
+        }
+        
+        if (amount <= 0) {
+            System.out.println("Ошибка: сумма должна быть положительной");
+            return false;
+        }
+        
+        if (this == to) {
+            System.out.println("Ошибка: нельзя переводить на тот же счет");
+            return false;
+        }
+        
+        return this.withdraw(amount) && to.deposit(amount);
+    }
+
     public double getBalance() {
-        return balance;
+        return this.balance;
     }
     
-    public int getAccountNumber() {
-        return accountNumber;
+    public String getAccountNumber() {
+        return this.accountNumber;
     }
 
     public Customer getOwner() {
-        return owner;
+        return this.owner;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Account account = new Account();
+        Customer customer = new Customer(0, "Gonsales");
+        Account account = new Account("1234 1234", 50000, customer);
+        Account account2 = new Account("5678 3456", 0, customer);
+
         account.deposit(100500);
         System.out.println("Баланс: " + account.getBalance());
+        account.transfer(account2, 150000);
+        System.out.println("Баланс счета: " + account.getAccountNumber() + " равен: " + account.getBalance());
+        System.out.println("Баланс счета: " + account2.getAccountNumber() + " равен: " + account.getBalance());
     }
 }
